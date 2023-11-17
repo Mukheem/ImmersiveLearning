48c3958724ef7760419ce09620710d0a71ac8094
Abdul Mukheem Shaik <mukheemuddin@gmail.com>
2023-11-17 01:25:16 +0100

    Basic connectivity with Arduino to Unity
    Basic connectivity with Arduino to Unity - Using Raw Code and no plugins

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class PlayerController : MonoBehaviour
+{
+    SerialPort arduinoPort = new SerialPort("/dev/cu.usbmodem11201", 9600);
+    // Start is called before the first frame update
+    void Start()
+    {
+        arduinoPort.DtrEnable = true;
+        arduinoPort.RtsEnable = true;
+        arduinoPort.Open();
+       arduinoPort.ReadTimeout = 50000;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (arduinoPort.IsOpen)
+        {
+            try
+            {
+                Debug.Log("Hey!");
+                Debug.Log(arduinoPort.ReadByte());
+                //Debug.Log(arduinoPort.ReadLine());
+                //arduinoPort.Close();
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        else
+        {
+            Debug.Log("Hey");
+        }
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
+    void OnApplicationQuit()
+    {
+        if (arduinoPort.IsOpen)
+        {
+            arduinoPort.Close();
+        }
+    }
+
+}
