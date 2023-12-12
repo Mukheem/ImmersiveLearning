0f3127cd48bc9314a16bfb0d24acbb40d13c8e65
akanksha <akankshabajaj11@gmail.com>
2023-12-13 00:41:45 +0100

    working on LightController Script

--- Assets/scripts/LightController.cs
+++ Assets/scripts/LightController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightController : MonoBehaviour
+
 {
     public Light roomLight;
     public float initialIntensity = 0.1f; // Adjust this value for the desired initial intensity
-    public float intensityChangeSpeed = 0.1f;
+    public float intensityChangeSpeed = 1.0f;
+    public Color yellowColor = Color.yellow;
+    public Color pinkColor = new Color(1.0f, 0.5f, 0.5f); // Adjust RGB values for the desired pink color
 
     // Start is called before the first frame update
     void Start()
@@ -14,26 +17,28 @@ public class LightController : MonoBehaviour
         Debug.Log("Light is off");
         // Set the initial intensity of the room light
         roomLight.intensity = initialIntensity;
+        // Set the initial color to pink
+        roomLight.color = pinkColor;
+        // Set ambient light to black
+        RenderSettings.ambientLight = Color.black;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                roomLight.intensity += intensityChangeSpeed * Time.deltaTime;
-                Debug.Log("Light is on. Current intensity: " + roomLight.intensity);
-            }
+            roomLight.intensity += intensityChangeSpeed * Time.deltaTime;
+            roomLight.color = yellowColor;
+            Debug.Log("Light is on. Current intensity: " + roomLight.intensity);
+        }
 
-            if (Input.GetKeyDown(KeyCode.DownArrow))  // Corrected line
-            {
-                roomLight.intensity -= intensityChangeSpeed * Time.deltaTime;
-                roomLight.intensity = Mathf.Max(0, roomLight.intensity);
-                Debug.Log("Light is off. Current intensity: " + roomLight.intensity);
-            }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            roomLight.intensity -= intensityChangeSpeed * Time.deltaTime;
+            roomLight.intensity = Mathf.Max(0, roomLight.intensity);
+            roomLight.color = pinkColor; // Maintain pinkColor
+            Debug.Log("Light is off. Current intensity: " + roomLight.intensity);
         }
     }
 }
