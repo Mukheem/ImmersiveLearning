3888b5dcb5439884815b2419b2d744fd52220986
Abdul Mukheem Shaik <mukheemuddin@gmail.com>
2023-12-31 16:06:40 +0100

    GomeScene created - Not functional

---
+++ Assets/Scripts/GameMenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GameMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadGravityScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
